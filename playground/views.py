d12d5395371621c338a864333f88ca547deb6e29
Sherzodd <85355488+Sherzodd@users.noreply.github.com>
2022-01-08 22:10:52 +0500

    Update views.py

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,24 @@
+from django.db.models.expressions import OrderBy
+from django.db.models.fields import DecimalField
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q, F, Value, Func, Count, ExpressionWrapper
+from django.db.models.functions import Concat
+from django.db.models.aggregates import Max, Min, Avg, Sum, Count
+from store.models import Collection, Customer, Product, OrderItem, Order
+
+
+
 
 def say_hello(request):
     
-    return render(request, 'index.html')
+    queryset = Product.objects.annotate(total_amount=Sum(
+        F('orderitem__unit_price') * F('orderitem__quantity')
+        )).order_by('-total_amount')[:5]
+    
+    return render(request, 'hello.html', {'name': 'Sher', 'result': list(queryset)})
 
+
+def home_page(request):
+    return HttpResponse('<h1>Home Page</h1>') 
